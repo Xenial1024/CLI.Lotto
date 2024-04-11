3884b389321e0737551c129f0e5975cf0b9d13c7
Xenial1024 <marko1024@seznam.cz>
2024-04-11 10:13:42 +0200

    Add project files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            byte randomNumber, guessedNumber;
+            int numberOfAttempts=0;
+            while (true)
+            {
+                Console.Write("Wpisz liczbę od 0 do 100: "); 
+                randomNumber = (byte)rnd.Next(0, 101);
+                numberOfAttempts++;
+                if (!byte.TryParse(Console.ReadLine(), out guessedNumber) || guessedNumber > 100)
+                {
+                    Console.WriteLine("Wpisano błędną wartość lub jej w ogóle nie wpisano. Spróbuj ponownie.");
+                    continue;
+                }
+                else if (guessedNumber > randomNumber)
+                    Console.WriteLine("Twoja liczba jest za duża. Wylosowana liczba to " + randomNumber + ".");
+                else if (guessedNumber < randomNumber)
+                    Console.WriteLine("Twoja liczba jest za mała. Wylosowana liczba to " + randomNumber + ".");
+                else
+                {
+                    Console.WriteLine($"Gratulacje. Odgadłeś wylosowaną liczbę w {numberOfAttempts} próbach");
+                    numberOfAttempts = 0;
+                }
+            }
+        }
+    }
+}
